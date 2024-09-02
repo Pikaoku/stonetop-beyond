d375a8905a427595fdb93d97c30d525b1fdbeafc
Paul O'Neill <pauloneill08@outlook.com>
2024-09-02 16:41:51 +0100

    Level required button moved

--- src/routes/(app)/admin/moves/new/+page.svelte
+++ src/routes/(app)/admin/moves/new/+page.svelte
@@ -9,27 +9,33 @@
 	};
 	$: console.log('creating new move');
 
+	let levelRequirementEnabled: boolean = false;
+
 	export let data;
 	$: ({ supabase } = data);
 
 	const create = async () => {
 		const { data, error } = await supabase.from('move').insert(createdMove);
 		console.log(data, error);
+		if (levelRequirementEnabled) {
+      createdMove.level_requirement = 6;
+    } else {
+      createdMove.level_requirement = 0; // or 0, depending on your requirements
+    }
 	};
 
 	const carta = new Carta();
 	$: console.log('classes', data.classes);
-
-	interface classOfMove {
-		class_id: number;
-		type: string;
-	}
+	$: console.log('level req', levelRequirementEnabled);
 </script>
 
 
 
 Creating new move
 <form on:submit={create} class="flex flex-col gap-2">
+
+	<input type="text" bind:value={createdMove.name} placeholder="Name" class="text-black">
+	<MarkdownEditor {carta} bind:value={createdMove.body} mode="tabs" theme="github"/>
 	<select class="text-black" bind:value={createdMove.type}>
 		<option value="">Select a class</option>
 		<option value="basic">Standard move</option>
@@ -45,13 +51,14 @@ Creating new move
 				<option value={c.id}>{c.name}</option>
 			{/each}
 		</select>
-
-
 	{/if}
-
-	<input type="text" bind:value={createdMove.name} placeholder="Name" class="text-black">
-	<MarkdownEditor {carta} bind:value={createdMove.body} mode="tabs" theme="github"/>
-	
+	<button class="toggle-button" on:click={() => levelRequirementEnabled = !levelRequirementEnabled}>
+		{#if levelRequirementEnabled}
+		  <span>Level 6+ Required: ON</span>
+		{:else}
+		  <span>Level 6+ Required: OFF</span>
+		{/if}
+	  </button>
 	<button type="submit">Confirm</button>
 </form>
 
